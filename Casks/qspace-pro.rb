c39298ba42f5dca91d4282f56554fd137c4a6ce8
xuelingkang <xuelingkang@163.com>
2022-07-09 11:34:09 +0800

    Qspace 2.9.16

--- Casks/qspace-pro.rb
+++ Casks/qspace-pro.rb
@@ -1,6 +1,6 @@
 cask "qspace-pro" do
-  version "2.9.14"
-  sha256 "184fce2ae0fd52cce829a3351970f9b16606561b9380ce1f423bd98c78c24bb3"
+  version "2.9.16"
+  sha256 "e83d986b0b143e379cf73b59f4bee7d89e219b4582235cc83875ec69eef40c28"
 
   url "https://cdn.awehunt.com/qs/rel/QSpace%20Pro_V#{version}.dmg"
 
