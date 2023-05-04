bebaac47a5b159535cb9b971912eafc733be8aca
xuelingkang <xuelingkang@163.com>
2023-05-04 20:46:04 +0800

    jdk-8u371

--- Casks/java8.rb
+++ Casks/java8.rb
@@ -1,14 +1,14 @@
 home = ENV["HOME"]
 cask 'java8' do
-  version '8u361,b09:0ae14417abb444ebb02b9815e2103550'
-  sha256 '34fda35f876d33edc0f9006d0a9c5bb45a3e42640053e0b2327ad6fb8dc249a7'
+  version '8u371,b09:ce59cff5c23f4e2eaf4e778a117d4c5b'
+  sha256 '413c857329f00009b24658e38425a5af41ebcabb11460410c039476a054dc5cd'
   url "file://#{home}/Downloads/jdk-#{version.before_comma}-macosx-x64.dmg"
   name 'Oracle Java 8 Standard Edition Development Kit'
   homepage 'https://www.oracle.com/technetwork/java/javase/overview/index.html'
 
   depends_on macos: '>= :el_capitan'
 
-  pkg 'JDK 8 Update 361.pkg'
+  pkg 'JDK 8 Update 371.pkg'
 
   uninstall pkgutil: "com.oracle.jdk#{version.before_comma}",
             delete:  [
